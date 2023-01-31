53d564f44c12aa7be1f956e44be619e892314ca5
Ludwika Malinowska <malinowska.L@wp.pl>
2023-01-31 18:19:58 +0100

    Dodano readme i poprawiono tworzenie bazy postgres na backendzie

--- rest-api/server-ps/src/index.js
+++ rest-api/server-ps/src/index.js
@@ -18,6 +18,26 @@ client
 	.connect()
 	.then(() => {
 		console.log("Connected to PostgreSQL")
+
+		client.query(`CREATE TABLE IF NOT EXISTS author (
+			id SERIAL PRIMARY KEY,
+			name VARCHAR(60) NOT NULL,
+			surname VARCHAR(60) NOT NULL,
+			dateOfBirth DATE NOT NULL,
+			writtenBooks VARCHAR(1000) NOT NULL 
+		  );
+
+		  
+		  CREATE TABLE IF NOT EXISTS book (
+			id SERIAL PRIMARY KEY,
+			title VARCHAR UNIQUE NOT NULL,
+			authorId INTEGER NULL, 
+			FOREIGN KEY (authorId) REFERENCES author (id),
+			publicationDate DATE NOT NULL
+		  );
+		  `);
+
+
 		const port = process.env.PORT || 5000
 		app.listen(port, () => {
 			console.log(`API server listening at http://localhost:${port}`)
