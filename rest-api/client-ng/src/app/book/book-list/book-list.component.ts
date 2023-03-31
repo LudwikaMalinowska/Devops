6bf0dfff6a38481cffd6f03f828eaa3951b0aeab
Ludwika Malinowska <malinowska.L@wp.pl>
2023-03-31 18:50:38 +0200

    Book - sorting

--- rest-api/client-ng/src/app/book/book-list/book-list.component.ts
+++ rest-api/client-ng/src/app/book/book-list/book-list.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Book, Author } from '../../interfaces/interfaces';
 // import { books, authors } from 'src/app/interfaces/data';
 import { Observable } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
 
 
 interface Response {
@@ -15,12 +16,14 @@ interface Response {
   styleUrls: ['./book-list.component.scss']
 })
 export class BookListComponent {
-
   books: Book[] = [];
+  sortForm!: FormGroup;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    let sort_type = "";
+    
     const req: Observable<Response> = this.http.get<Response>("http://localhost:5000/api/books")
     req.subscribe((val: Response) => {
       console.log(val)
@@ -30,7 +33,23 @@ export class BookListComponent {
       // else {
       //   this.books = books;
       // }
-      
     });
+
+    this.sortForm = new FormGroup({
+      'sort_type': new FormControl(sort_type),
+    })
+  }
+
+  changeSortType(){
+    const sort_type = this.sortForm.value['sort_type'];
+
+    switch(sort_type){
+      case "sort_ascending_title":
+        this.books.sort((a, b) => a.title.localeCompare(b.title));
+        break;
+      case "sort_descending_title":
+        this.books.sort((a, b) => b.title.localeCompare(a.title));
+    }
+    
   }
 }
